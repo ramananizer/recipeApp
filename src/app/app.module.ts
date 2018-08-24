14ff65d0b812567182b5e62fd49bda2b4757efbc
Raman Jha <ramananizer@hotmail.com>
2018-08-24 11:48:30 +0530

    first commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,37 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { ErrorHandler, NgModule } from '@angular/core';
+import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
+import { SplashScreen } from '@ionic-native/splash-screen';
+import { StatusBar } from '@ionic-native/status-bar';
+import { Pro } from '@ionic/pro';
+import { MyApp } from './app.component';
+
+import {EditRecipePageModule} from '../pages/edit-recipe/edit-recipe.module';
+import {RecipePageModule} from '../pages/recipe/recipe.module';
+import{RecipesPageModule} from '../pages/recipes/recipes.module';
+import{ShoppingListPageModule} from '../pages/shopping-list/shopping-list.module';
+import{TabsPageModule} from '../pages/tabs/tabs.module';
+Pro.init('D31B0942', {
+  appVersion: '0.0.1'
+})
+
+@NgModule({
+  declarations: [
+    MyApp
+  ],
+  imports: [
+    BrowserModule,
+    IonicModule.forRoot(MyApp),EditRecipePageModule,RecipePageModule, RecipesPageModule,
+    ShoppingListPageModule, TabsPageModule
+  ],
+  bootstrap: [IonicApp],
+  entryComponents: [
+    MyApp
+  ],
+  providers: [
+    StatusBar,
+    SplashScreen,
+    {provide: ErrorHandler, useClass: IonicErrorHandler}
+  ]
+})
+export class AppModule {}
---
+++ src/pages/shopping-list/shopping-list.ts
@@ -0,0 +1,31 @@
+import { Component } from '@angular/core';
+import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { NgForm } from '@angular/forms';
+
+/**
+ * Generated class for the ShoppingListPage page.
+ *
+ * See https://ionicframework.com/docs/components/#navigation for more info on
+ * Ionic pages and navigation.
+ */
+
+@IonicPage()
+@Component({
+  selector: 'page-shopping-list',
+  templateUrl: 'shopping-list.html',
+})
+export class ShoppingListPage {
+
+  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  }
+
+  onAddItem(f :NgForm){
+    console.log(f);
+
+  }
+
+  ionViewDidLoad() {
+    console.log('ionViewDidLoad ShoppingListPage');
+  }
+
+}
